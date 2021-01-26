3c342660ddda76b1940a9842f4c39f7e87a8e169
samuelceron <samceronm@gmail.com>
2021-01-26 12:19:42 -0600

    Creating and appending axes

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,36 @@ let svg = d3
 
 let chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+//Import Data
+d3.csv("assets/data/data.csv").then(function(data){
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    data.forEach(function(d){
+      d.poverty = +d.poverty
+      d.healthcare = +d.healthcare
+    });
+    // Step 2: Create scale functions
+    // ==============================
+    let xLinearScale = d3.scaleLinear()
+    .domain(d3.extent([d3.min(data, d=>d.poverty) -1 ,d3.max(data, d=> d.poverty)])).nice()
+    .range([0, width])
+
+    let yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d=>d.healthcare) + 2])
+    .range([height, 0])
+
+    // Step 3: Create axis functions
+    // ==============================
+    let xAxis = d3.axisBottom(xLinearScale).ticks((d3.max(data, d=> d.poverty) -d3.min(data, d=>d.poverty)) / 2 )
+    let yAxis = d3.axisLeft(yLinearScale)
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis)
+
+    chartGroup.append("g")
+    .call(yAxis)
+});
